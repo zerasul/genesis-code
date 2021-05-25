6da1f0b629b695dfc9c5f303c725676d80c67214
zerasul <zerasul@gmail.com>
2021-05-25 23:11:32 +0200

    adding palette reading (first version)

--- src/imagePreviewProvider.ts
+++ src/imagePreviewProvider.ts
@@ -1,9 +1,8 @@
-import { readFileSync } from 'fs';
+import { readFileSync,createReadStream } from 'fs';
 import * as Path from 'path';
 import * as imagesize from 'image-size';
 import * as vscode from 'vscode';
-var palette = require('get-rgba-palette');
-var pixels = require('get-image-pixels');
+let imageJS = require('imagejs');
 
 
 
@@ -18,7 +17,8 @@ export class ImagePreviewDocument extends vscode.Disposable implements vscode.Cu
         let imagesizes = imagesize.imageSize(this.imagedata);
         this.width=imagesizes.width;
         this.height=imagesizes.height;
-        
+        this.bitmap = new imageJS.Bitmap();
+      
     }
 
     uri: vscode.Uri;
@@ -27,6 +27,8 @@ export class ImagePreviewDocument extends vscode.Disposable implements vscode.Cu
     imagedatab64:any;
     imagedata:Buffer;
     filename:String;
+    bitmap: any;
+    palette: any;
 
     private static _disposable(){
         
@@ -56,6 +58,7 @@ export class ImagePreviewProvider implements vscode.CustomReadonlyEditorProvider
     }
     resolveCustomEditor(document: ImagePreviewDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
         let imageuri="data:image/png;base64 ,"+document.imagedatab64;
+        
         let width = document.width;
         let height = document.height;
         let tilesw = (document.width!==undefined)?document.width/8:0;
@@ -69,7 +72,51 @@ export class ImagePreviewProvider implements vscode.CustomReadonlyEditorProvider
         htmltemplate=htmltemplate.replace(new RegExp("{{height}}", 'g'),String(height));
         htmltemplate=htmltemplate.replace(new RegExp("{{tilesw}}", 'g'),String(tilesw));
         htmltemplate=htmltemplate.replace(new RegExp("{{tilesh}}", 'g'),String(tilesh));
-        
+        document.bitmap.readFile(document.uri.fsPath).then(() =>{
+            this.parsePalette(document.bitmap,webviewPanel.webview);
+        })
+        .catch(() =>{
+            console.log("ERROR");
+        });
         webviewPanel.webview.html=htmltemplate;
+    }
+
+    private parsePalette(bitmap: any, webView:vscode.Webview){
+        let palette = new Set<String>();
+        for (let index = 0; index < bitmap.width;index++){
+            for(let index2=0; index2<bitmap.height;index2++){
+                let color = bitmap.getPixel(index,index2);
+                palette.add(JSON.stringify(color));
+            }
+        }
+        let htmlcolours:string=this.generateHtmlGrid(palette);
+        let html=webView.html;
+        html=html.replace(new RegExp("Loading Palette...",'g'), htmlcolours);
+        webView.html=html;  
+    }
+
+    private generateHtmlGrid(palette:Set<String>):string{
+        let html:string=`<p>${palette.size} Colours</p>`;
+        html+="<div class='grid-container'>";
+        palette.forEach(element=> {
+            let {a,r,g,b}=JSON.parse(element as string);
+            let htmlelement=`<div class="grid-element" style="background-color:rgba(${r},${g},${b},${a});"></div>`;
+            html+=htmlelement;
+        });
+        html+="</div>";
+        return html;
+    }
+   
+}
+class ColorPalette {
+
+    constructor(public r:number,
+        public g:number,
+        public b:number) {
+        
+    }
+
+    public equals(another:ColorPalette):boolean{
+        return JSON.stringify(this)===JSON.stringify(another);
     }
 }
